c26a0495c988b9039c67a903d36167978f339a14
Yi-Chen <alvin9840@gmail.com>
2025-09-24 01:02:24 -0400

    feat: fetch all posts API integration

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find()
-            .populate('createdBy');
+            .populate('createdBy').sort({ createdAt: -1 });
         res.json(posts)
     } catch (err) {
         res.status(500).json({ message: err.message })
